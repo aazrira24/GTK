d04d26cfda7fe1084c5943b4c778c256e3b0a0f7
Anas <aazrira24@gmail.com>
2024-12-31 14:00:30 +0100

    Create Button.h

---
+++ Les_Sdds/Button.h
@@ -0,0 +1,31 @@
+#include "C:\Users\LENOVO\Desktop\GTK\GTK\Les Sdds\ComboBox.h"
+
+
+
+
+/*
+    Structure of Buttons
+*/
+
+
+#define _Mnemonic 0
+#define _Always_Show_Image 0
+#define _Mnemonic_Car ' '
+#define _Label  NULL
+#define _Image NULL
+#define _Color NULL
+
+
+typedef struct 
+{
+    GtkWidget *Ptr_Button;
+    char *Id;
+    bool Mnemonic;
+    bool Always_Show_Image;
+    Position Position_;
+    char Mnemonic_Car;
+    char *Label;
+    char *Image;
+    char *Color;
+    Taille Size;
+}Button;
